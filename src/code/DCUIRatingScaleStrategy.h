409ca5e748a0a80d48190d4bc9d38c845a7777ef
drekka <d4rkf1br3@gmail.com>
2010-10-04 21:05:44 +1100

    Attempting to fix xcode 4 stuff up

---
+++ src/code/DCUIRatingScaleStrategy.h
@@ -0,0 +1,48 @@
+//
+//  DCUIRatingScaleStrategy.h
+//  dUsefulStuff
+//
+//  Created by Derek Clarkson on 10/3/10.
+//  Copyright (c) 2010 Oakton Pty Ltd. All rights reserved.
+//  
+
+#import <UIKit/UIKit.h>  
+
+/**
+ * This interface outlines the contract for a class which handles scale
+ * related functionality for a DCUIRating.
+ */
+@protocol DCUIRatingScaleStrategy
+ssss
+/** \name Constructor */
+-(id) initWithOffImage:(UIImage *) aOffImage onImage:(UIImage *) aOnImage halfOnImage:(UIImage *) aHalfOnImage;
+
+/** \name Methods */
+
+/**
+ * Called during DCUIRating:drawRect, this is where the strategy is 
+ * expected to handle drawing the correct image on the display.
+ */
+-(void) drawImageAtIndex:(int) index;
+
+/**
+ * Asks the strategy to calculate a new rating based on the touch.
+ */
+-(float) calcNewRatingFromTouchX:(int) touchX;
+
+/**
+ * Setter which allows setting of the current value.
+ */
+-(void) setRating:(float) aRating;
+
+/**
+ * Getter for the current rating.
+ */
+-(float) rating;
+
+/**
+ * Used to get a formatted version of the rating suitable for displays.
+ */
+-(NSString *) formattedRating;
+
+@end
---
+++ src/tests/DCUIRatingScale5StrategyTests.h
@@ -0,0 +1,16 @@
+//
+//  DCUIRatingScale5StrategyTests.h
+//  dUsefulStuff
+//
+//  Created by Derek Clarkson on 10/3/10.
+//  Copyright (c) 2010 Oakton Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DCUIRatingScale5StrategyTests : NSObject {
+
+}
+
+@end
